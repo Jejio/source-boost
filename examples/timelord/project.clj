1607e30573e438596fff3eaefe1207d5a2d34b77
Jejio <104919310+Jejio@users.noreply.github.com>
2021-01-26 13:35:35 +0000

    Version 1.1r36

--- examples/timelord/project.clj
+++ examples/timelord/project.clj
@@ -1,4 +1,8 @@
 (defproject timelord "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :license {:name
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+                 [org.clojure/clojurescript "0.0-3196"
