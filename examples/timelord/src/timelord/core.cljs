ad94f4b9c6cc46df50c1b0ca167a33c9ad4cac3a
Jejio <104919310+Jejio@users.noreply.github.com>
2021-03-11 22:47:12 +0000

    fu

--- examples/timelord/src/timelord/core.cljs
+++ examples/timelord/src/timelord/core.cljs
@@ -1,1 +1,4 @@
-(ns ^:figwheel-always timel
+(ns ^:figwheel-always timelord.core
+  (:require [reagent.core :as reagent :refer [atom]]
+            [jamesmacaulay.zelkova.signal :as z]
+            [jamesmacaulay.zelkova.impl.signal 
