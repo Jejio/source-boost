7991f21f72a08d1e5818251097d83aaa05ed62cc
Jejio <104919310+Jejio@users.noreply.github.com>
2021-03-22 10:08:49 +0000

    4.9

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject jamesmacaulay/zelkova "0.4.0"
   :description "Elm-style FRP for Clojure and ClojureScript"
   :url "http://github.com/jamesmacaulay/zelkova"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/c
