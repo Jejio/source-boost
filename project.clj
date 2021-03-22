5fd765d20f42c0a03c2385dc8235edf52949e49c
Jejio <104919310+Jejio@users.noreply.github.com>
2021-03-22 12:06:33 +0000

    Changes!

--- project.clj
+++ project.clj
@@ -4,4 +4,7 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/c
+                 [org.clojure/clojurescript "0.0-3308"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
+  :plugins [[lein-cljsbuild "1.0.5"]
+            [com.cemerick/clojurescript.test
